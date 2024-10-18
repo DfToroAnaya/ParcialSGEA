7e511c2a3d9e8807322747184116048c195d2072
Kevin-SantiagoC <santiagocastano47@gmail.com>
2024-10-17 23:53:00 -0500

    Exhibitions list

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ArtistController;
+use App\Http\Controllers\ExhibitionController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,12 +18,19 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    //Artistas
     Route::get('/artists', [ArtistController::class, 'index'])->name('artists.index');
     Route::post('/artists', [ArtistController::class, 'store'])->name('artists.store');
     Route::get('/artists/create', [ArtistController::class, 'create'])->name('artists.create');
     Route::delete('/artists/{artist}', [ArtistController::class, 'destroy'])->name('artists.destroy');
     Route::put('/artists/{artist}', [ArtistController::class, 'update'])->name('artists.update');
     Route::get('/artists/{artist}/edit', [ArtistController::class, 'edit'])->name('artists.edit');
+
+
+    //Exposiciones 
+    Route::get('/exhibitions', [ExhibitionController::class, 'index'])->name('exhibitions.index');
+
 
 
 });
