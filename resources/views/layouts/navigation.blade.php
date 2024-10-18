8b415439200aaf6b20ea3411558c1cc2fe903ff4
Kevin-SantiagoC <santiagocastano47@gmail.com>
2024-10-17 23:03:35 -0500

    Artist View

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,11 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('artists.index')" :active="request()->routeIs('artists.index')">
+                        {{ __('Artist') }}
+                    </x-nav-link>
+                    </div>
                 </div>
             </div>
 
@@ -69,6 +74,10 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('artists.index')" :active="request()->routeIs('artists.index')">
+                {{ __('Artists') }}
             </x-responsive-nav-link>
         </div>
 
