724e4b7408d8ca322f25b9f89d428b79a6dd5d19
Diego Toro <dtoroanaya16@gmail.com>
2024-10-18 23:23:01 -0500

    Controller Complate

--- app/Http/Controllers/WorkController.php
+++ app/Http/Controllers/WorkController.php
@@ -11,7 +11,11 @@ class WorkController extends Controller
      */
     public function index()
     {
-        //
+        $obras = DB::table('works')
+        ->join('artists', 'works.id', '=', 'artists.id')
+        ->select('works.*','artists.artista_id')
+        ->get();
+        return view ('obra.index', ['obras' => $obras]);
     }
 
     /**
@@ -19,7 +23,10 @@ class WorkController extends Controller
      */
     public function create()
     {
-        //
+        $obras = DB::table('artist')
+        ->orderBy('nombre')
+        ->get();
+        return view('obra.new', ['obras' => $obras]);
     }
 
     /**
@@ -27,7 +34,16 @@ class WorkController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $obra = new work();
+        $obra->nombre = $request->name;
+        $obra->id = $request->code;
+        $obra->save();
+
+        $obra = DB::table('works')
+        ->join('artists', 'works.id', '=', 'artists.id')
+        ->select('works.*', "artists.nombre")
+        ->get();
+        return view('obra.index', ['obras' => $obra]);
     }
 
     /**
@@ -43,7 +59,11 @@ class WorkController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $obra = work::find($id);
+        $artistas = DB::table('artists')
+        ->orderBy('nombre')
+        ->get();
+        return view('obra.edit', ['obra' =>$obra, 'artistas' => $artistas]);
     }
 
     /**
@@ -51,7 +71,18 @@ class WorkController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $obra = work::find($id);
+
+        $obra->titulo = $request->name;
+        $obra->id = $request->code;
+        $obra->save();
+
+        $obras = DB::table('works')
+        ->join('srtists', 'works.id', '=', 'artists.id')
+        ->select('works.*', "artists.nombre")
+        ->get();
+
+        return view('obra.index', ['obras' => $obras]);
     }
 
     /**
@@ -59,6 +90,14 @@ class WorkController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $obra = work::find($id);
+        $obra->delete();
+
+        $obra = DB::table('works')
+        ->join('artists', 'works.id', '=', 'artists.id')
+        ->select('works.*', "artists.nombre")
+        ->get();
+
+        return view('obra.index', ['obras' => $obra]);
     }
 }
--- app/Models/work.php
+++ app/Models/work.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class work extends Model
 {
-    //
+    use HasFactory;
+    protected $table = 'works';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
 }
